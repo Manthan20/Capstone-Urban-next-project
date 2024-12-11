528367d8b962c505d2af34ef5e3f096cd9abe654
varshini2104 <varshini2104@gmail.com>
2024-12-11 15:29:08 -0500

    Add Map Component for Property Details Page

--- src/components/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -8,7 +8,7 @@ function PropertyCard({ property }) {
     const savedWishlist = localStorage.getItem('wishlist');
     return savedWishlist ? JSON.parse(savedWishlist) : [];
   });
-  
+
   const handleWishlistToggle = (property) => {
     setWishlist((prevWishlist) => {
       const newWishlist = prevWishlist.includes(property.id)
@@ -23,31 +23,35 @@ function PropertyCard({ property }) {
 
   return (
     <div className="property-card" key={property.id}>
-      <Link to={`/property/${property.id}`} className="property-link">
-        <img
-          src={property.imageUrl.split(',')[0]}
-          alt={property.title}
-          className="property-image"
-          style={{ height: '250px', width: '300px' }}
-        />
-        <h2 className="property-title">{property.title}</h2>
-        <p className="property-details">
-          <strong>Price:</strong>  ${property.price}
-        </p>
-        <p className="property-details">
-          <strong>Length:</strong>  {property.length} m |  <strong>Breadth:</strong> {property.breadth} m
-        </p>
-        <p className="property-location">
-          {property.city}, {property.state}, {property.pincode}
-        </p>
-      </Link>
-      <button
-        className={`wishlist-button ${wishlist.includes(property.id) ? 'active' : ''
-          }`}
-        onClick={() => handleWishlistToggle(property)}
-      >
-        ♥
-      </button>
+      <img
+        src={property.imageUrl.split(',')[0]}
+        alt={property.title}
+        className="property-image"
+        style={{ height: '250px' }}
+      />
+      <h2 className="property-title">{property.title}</h2>
+      <p className="property-details">
+        <strong>Price:</strong>  ${property.price}
+      </p>
+      <p className="property-details">
+        <strong>Length:</strong>  {property.length} m |  <strong>Breadth:</strong> {property.breadth} m
+      </p>
+      <p className="property-location">
+        {property.city}, {property.state}, {property.pincode}
+      </p>
+      <div className='buttonGroup'>
+        <button
+          className={`wishlist-button ${wishlist.includes(property.id) ? 'active' : ''
+            }`}
+          onClick={() => handleWishlistToggle(property)}
+        >
+          ♥
+        </button>
+        <Link to={`/property/${property.id}`} className="property-link">
+          Details
+        </Link>
+
+      </div>
     </div>
   );
 }
