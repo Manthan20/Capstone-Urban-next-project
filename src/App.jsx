b09177987531234d862b67c58a8ecf56b230bfed
amrit8169@gmail.com <Manthan20@gmail.com>
2024-12-11 15:16:39 -0500

    Add Dark Light Mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import ListingPage from "./routes/listingPage/listingPage";
 import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/singlePage/singlePage";
 import Login from "./routes/login/login";
-import Signup from "./routes/signup/signup";
 import AddProperty from "./routes/admin/AddProperty/AddProperty";
 import Dashboard from "./routes/admin/Dashboard/Dashboard";
 import PropertiesPage from "./routes/PropertiesPage/PropertiesPage";
@@ -13,8 +12,13 @@ import AboutUsPage from "./routes/AboutUsPage/AboutUsPage";
 import UpdateProperty from "./routes/admin/UpdateProperty/UpdateProperty";
 import JoinMembershipPage from "./routes/JoinMembershipPage/JoinMembershipPage";
 import PropertyDetailsPage from "./routes/PropertyDetailsPage/PropertyDetailsPage";
-import ProfilePage from "./routes/profilePage/profilePage";
+import ContactUsPage from "./routes/ContactUsPage/ContactUsPage";
+import ProfilePage from "./routes/ProfilePage/ProfilePage";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import AdminMessagesPanel from "./routes/admin/AdminMessagesPanel/AdminMessagesPanel";
+import AdminAppointments from "./routes/admin/Appointments/Appointments";
+import AppointmentCalendar from "./routes/AppointmentCalendar/AppointmentCalendar";
+import Signup from "./routes/Signup/Signup";
 
 function App() {
   const router = createBrowserRouter([
@@ -51,8 +55,12 @@ function App() {
           element: <UpdateProperty />,
         },
         {
-          path: "/profile/*",
-          element:<ProtectedRoute ><ProfilePage /></ProtectedRoute>,
+          path: "/admin/messages",
+          element: <AdminMessagesPanel />,
+        },
+        {
+          path: "/admin/appointments",
+          element: <AdminAppointments />,
         },
         {
           path: "/properties",
@@ -71,8 +79,20 @@ function App() {
           element: <AboutUsPage />,
         },
         {
+          path: "/appointment",
+          element: <AppointmentCalendar />,
+        },
+        {
           path: "/join",
           element: <JoinMembershipPage />,
+        },
+        {
+          path: "/profile/*",
+          element:<ProtectedRoute ><ProfilePage /></ProtectedRoute>,
+        },
+        {
+          path: "/contact",
+          element: <ContactUsPage />,
         },
         {
           path: "/:id",
